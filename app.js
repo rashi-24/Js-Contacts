8a60bf5df24784f17535459c348fdcee6f295f36
rashi-24 <sharma.rashi2408@gmail.com>
2022-07-20 15:56:00 +0530

    rashi

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
-const btn = document.querySelector('#btnGet');
-
-btn.onclick = function() {
-
-    var promise = new Promise((reslove, reject) => {
-        var request = new XMLHttpRequest()
-        request.open('GET', 'https://jsonplaceholder.typicode.com/users')
-        request.onload = () => {
-            if (request.status == 200) {
-                reslove(request.response)
-            } else {
-                reject(Error(request.statusText))
-            }
-        }
-        request.send();
-    })
-
-    promise.then((data) => {
-        console.log(data)
-        console.log('Got data! Promise fulfilled.');
-        const result = JSON.parse(data)
-        var player = '<h2>Lists of Users</h2>';
+async function getUserList() {
+    try {
+        let res = await fetch('https://jsonplaceholder.typicode.com/users');
+        let result = await res.json();
+        let divmain = document.getElementById('message');
+        var player = document.createElement('h2');
+        player.textContent = "Lists of Users";
+        divmain.appendChild(player);
         result.forEach(function(user) {
-            player +=
-                `<div class="player">
-                  <div class="strength">Name : ${user.name}</div>
-                  <div>Email   : ${user.email}</div>
-                  <div>Phone   : ${user.phone}</div>
-                  <div>Website : ${user.website}</div>
-                  <div>Company : ${user.company.name}</div>
-                  <div>City    : ${user.address.city}</div>
-                  <div>Zipcode : ${user.address.zipcode}</div>
-                 </div>`
-        });
-
-        message.innerHTML = player;
-    }, (error) => {
+            let divout = document.createElement('div');
+            divout.className = 'player';
+            divmain.appendChild(divout);
+            let divin1 = document.createElement('div');
+            divin1.className = 'strength';
+            divin1.innerText = `Name : ${user.name}`;
+            divout.appendChild(divin1);
+            let divin2 = document.createElement('div');
+            divin2.innerText = `Email   : ${user.email}`;
+            divout.appendChild(divin2);
+            let divin3 = document.createElement('div');
+            divin3.innerText = `Phone   : ${user.phone}`;
+            divout.appendChild(divin3);
+            let divin4 = document.createElement('div');
+            divin4.innerText = `Website : ${user.website}`;
+            divout.appendChild(divin4);
+            let divin5 = document.createElement('div');
+            divin5.innerText = `Company : ${user.company.name}`;
+            divout.appendChild(divin5);
+            let divin6 = document.createElement('div');
+            divin6.innerText = `City    : ${user.address.city}`;
+            divout.appendChild(divin6);
+            let divin7 = document.createElement('div');
+            divin7.innerText = `Zipcode : ${user.address.zipcode}`;
+            divout.appendChild(divin7);
+        })
+    } catch (error) {
         console.log('Promise rejected.');
         console.log(error.message);
-    });
+    }
 }
